37122dbe4163977efb66fc75c1de0fb73c1bbe60
shubhadip <shubhadip.maity@gmail.com>
2022-09-11 09:27:01 +0530

    refactor: chore changes

--- jest.config.ts
+++ jest.config.ts
@@ -36,12 +36,12 @@ export default {
   // coverageProvider: "babel",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: [
+    "json",
+    "text",
+    "lcov",
+    "clover"
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
